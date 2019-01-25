5d13f65cfbf8527e06deec00742bc9823953e3f3
Tyler Hancock <darklime@live.ca>
2019-01-24 23:19:25 -0700

    Added base mod class.

---
+++ src/main/java/com/getconfluxed/itemmodifiers/ItemModifiersMod.java
@@ -0,0 +1,14 @@
+package com.getconfluxed.itemmodifiers;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "itemmodifiers", name = "Item Modifiers", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
+public class ItemModifiersMod {
+
+    @EventHandler
+    public void onPreInit (FMLPreInitializationEvent event) {
+    	
+    }
+}
